fecff1d6f41590db9ceef40a158f629c29522852
martscribeLaptop <martscribe@gmail.com>
2023-01-30 19:34:46 -0600

    fixed identation problems

--- app.js
+++ app.js
@@ -17,3 +17,20 @@ btnSi.addEventListener('click', function(e) {
     const cancion = new Audio('modo_hot.mp3');
     cancion.play();
 });
+divModoHot.addEventListener('click', function(e) {
+    divModoHot.innerHTML = "<img src='https://i.pinimg.com/originals/c5/e3/c9/c5e3c9b5260daa31f5a4ab03ff048ece.png'>";
+})
+divModoHot.addEventListener('click', function(e) { 
+    const img = document.createElement("img");
+    //querySelector te busca un elemento aunque no tenga id o class
+    img.src = "https://i.pinimg.com/originals/c5/e3/c9/c5e3c9b5260daa31f5a4ab03ff048ece.png";
+    divModoHot.appendchild(img);
+})
+ //divModoHot.appendChild = "<img src='https://i.pinimg.com/originals/c5/e3/c9/c5e3c9b5260daa31f5a4ab03ff048ece.png'>"; no funcionaria solito ya que habira que crear un nodo de imagen qu elo pueda contener primero pero si haces inner html sobreescribe otros elementos que esten dentro en cambio con apend child solo agrega el elemento como imagen
+botones = document.getElementsByTagName("button");
+console.log(botones);
+
+window.addEventListener('beforeunload', (event) => {
+    event.preventDefault()
+    event.returnValue = ""
+})
